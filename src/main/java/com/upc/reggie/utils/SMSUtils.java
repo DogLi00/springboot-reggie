4d49dbe470260a61b5878efb84467eb190305cfe
DogLi00 <139725756+DogLi00@users.noreply.github.com>
2023-07-18 01:15:11 +0800

    Update SMSUtils.java
    删除了阿里云的key和value，泄露了也没事估计，只能发送到绑定手机号。【阿里牛逼，发邮件提示密码泄露。】

--- src/main/java/com/upc/reggie/utils/SMSUtils.java
+++ src/main/java/com/upc/reggie/utils/SMSUtils.java
@@ -15,9 +15,9 @@ public class SMSUtils {
 	public static com.aliyun.dysmsapi20170525.Client createClient() throws Exception {
 		com.aliyun.teaopenapi.models.Config config = new com.aliyun.teaopenapi.models.Config()
 				// 必填，您的 AccessKey ID
-				.setAccessKeyId("LTAI5tEcqRWAX6moeoPyofEi")
+				.setAccessKeyId("*")
 				// 必填，您的 AccessKey Secret
-				.setAccessKeySecret("rJ4OrVD7g7S2f3tGJVj1tdvkHaYwiU");
+				.setAccessKeySecret("*");
 		// 访问的域名
 		config.endpoint = "dysmsapi.aliyuncs.com";
 		return new com.aliyun.dysmsapi20170525.Client(config);
@@ -41,7 +41,7 @@ public class SMSUtils {
 		System.out.println(phoneNumbers.equals("15066066019"));
 		// 请确保代码运行环境设置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_ID 和 ALIBABA_CLOUD_ACCESS_KEY_SECRET。
 		// 工程代码泄露可能会导致 AccessKey 泄露，并威胁账号下所有资源的安全性。以下代码示例使用环境变量获取 AccessKey 的方式进行调用，仅供参考，建议使用更安全的 STS 方式，更多鉴权访问方式请参见：https://help.aliyun.com/document_detail/378657.html
-		com.aliyun.dysmsapi20170525.Client client = SMSUtils.createClient("LTAI5tEcqRWAX6moeoPyofEi","rJ4OrVD7g7S2f3tGJVj1tdvkHaYwiU");
+		com.aliyun.dysmsapi20170525.Client client = SMSUtils.createClient("*","*");
 		com.aliyun.dysmsapi20170525.models.SendSmsRequest sendSmsRequest = new com.aliyun.dysmsapi20170525.models.SendSmsRequest()
 				.setSignName("阿里云短信测试")
 				.setTemplateCode("SMS_154950909")
